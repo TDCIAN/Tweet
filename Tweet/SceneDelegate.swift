260794a26ba7c3d8f38b8f655133779cd08f88c7
TDCIAN <tdcian71@gmail.com>
2022-03-22 08:38:28 +0900

    config basic settings

---
+++ Tweet/SceneDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  SceneDelegate.swift
+//  Tweet
+//
+//  Created by JeongminKim on 2022/03/21.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = UINavigationController(rootViewController: ViewController())
+        window?.backgroundColor = .systemBackground
+        window?.makeKeyAndVisible()
+    }
+}
---
+++ Tweet/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Tweet
+//
+//  Created by JeongminKim on 2022/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
